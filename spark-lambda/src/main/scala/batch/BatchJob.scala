3b407a0214987b383106b543d82a99bab3569830
Matthew Gerstemeier <matthew.gerstemeier@nationwide.com>
2018-05-14 18:46:48 -0400

    committing test code to validate creation of spark instance

--- spark-lambda/src/main/scala/batch/BatchJob.scala
+++ spark-lambda/src/main/scala/batch/BatchJob.scala
@@ -1,5 +1,18 @@
 package batch
 
+import java.lang.management.ManagementFactory
+
+import org.apache.spark.{SparkContext, SparkConf}
+
 object BatchJob {
+
+  def main (args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setMaster("local").setAppName("Lambda with Spark")
+
+    val sc = new SparkContext(conf)
+
+    println(sc)
+  }
 
 }
